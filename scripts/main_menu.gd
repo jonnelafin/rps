1fc1187c50f055d1f96382ce43737f5c64233b8d
jonnelafin <elias.eelinen@gmail.com>
2020-06-21 13:34:39 +0300

    new build

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -8,7 +8,9 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	if(OS.get_name() == "HTML5"):
+		$Label2.visible = false
+	#pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
